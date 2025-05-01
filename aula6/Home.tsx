739fb3a8c73d56d8d63358f30ef25180f28191ab
Nicolas Oliveira da Silva <128194431+NicolasOliveira01@users.noreply.github.com>
2025-05-01 20:23:22 -0300

    Delete aula6 directory

--- aula6/Home.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Home() {
-    return (
-        <View>
-            <Text> Essa é a tela Home</Text>
-        </View>
-    )
-}
--- aula6/Profile.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Profile() {
-    return (
-        <View>
-            <Text> Essa é a tela Perfil</Text>
-        </View>
-    )
-}
